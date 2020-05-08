77eb362b16136f732c1eafe5774b9cfa60450b4b
nwporsch <porschnickalas@gmail.com>
2020-05-08 01:32:44 -0500

    Solution1 results

--- 3way-openmp/openmp.sh
+++ 3way-openmp/openmp.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-#for i in 1 2 4 8 16
-#do
-i=1
-	#for j in 1 2 3 4 5 6 7 8 9 10	
-	#do
-		sbatch --constraint=elves --time=04:00:00 --ntasks-per-node=$i --nodes=1 --mem-per-cpu=$((16/$i))G --partition=killable.q start.sbatch
-#	done
+threads='1 2 4 6 16 32'
+ncores='1 2 4 8 16 32'
+problem='1000 5000 100000 500000 1000000'
 
-#done
+for i in $ncores
+do
+	for j in $problem
+	do
+		for k in $threads
+		do
+			sbatch --constraint=dwarves --mail-type=END --time=1:00:00 --mem=32G --cpus-per-task=$i start.sh $j $k -o "OpenMP_dwarves_CORES:$i_Problem:$j_threads:$k"
+		done
+	done
+done
