42522ec4caa475698a4e1ac4ee0ca1ede78573c2
a-p-j <apjavier@ksu.edu>
2020-05-10 16:21:14 -0500

    fixed typo

--- 3way-pthreads/pthreadsolution.c
+++ 3way-pthreads/pthreadsolution.c
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]){
 
 	//See if arguments were passed
 	if(argc == 3){
-		pthread_mutext_init(&mutexsum, NULL);
+		pthread_mutex_init(&mutexsum, NULL);
 
 		NUM_ENTRIES = strtol(argv[1], NULL, 10);
 		NUM_THREADS = strtol(argv[2], NULL, 10);
