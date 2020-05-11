c8ae147ffb960e3660a02a61bb4a631cbd18db19
nwporsch <porschnickalas@gmail.com>
2020-05-11 14:13:18 -0500

    Created a Makefile and fixed the script files

---
+++ 3way-mpi/mpi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+threads='1 2 4 8 16 32'
+ncores='1 2 4 8 16 32'
+problem='1000 5000 100000 500000 1000000'
+
+for i in $ncores
+do
+	for j in $problem
+	do
+		for k in $threads
+		do
+			sbatch --constraint=dwarves --mail-type=END --time=1:00:00 --mem=32G --cpus-per-task=$i --output=$j.$k.$i.%J.out start.sh $j $k
+		done
+	done
+done
